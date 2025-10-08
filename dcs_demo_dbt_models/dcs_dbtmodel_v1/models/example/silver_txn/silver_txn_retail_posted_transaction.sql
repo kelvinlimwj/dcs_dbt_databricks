91b9b58d435630cc518231e45c692614080b5779
kelvinlimwj <kelvinlimwj>
2025-10-08 06:11:23 +0000

    Updated to include generic test

--- dcs_demo_dbt_models/dcs_dbtmodel_v1/models/example/silver_txn/silver_txn_retail_posted_transaction.sql
+++ dcs_demo_dbt_models/dcs_dbtmodel_v1/models/example/silver_txn/silver_txn_retail_posted_transaction.sql
@@ -1,14 +1,49 @@
 {{ config(
-    schema='dcs_silver_txn',
+    schema = 'dcs_silver_txn',
     tags=['silver']
 ) }}
 
-with source_data as (
+WITH source_data AS (
+    SELECT *
+    FROM {{ ref('silver_txn_full_posted_transaction') }}
+    WHERE posting_transaction_code IN ('GL491', 'IZ106', 'UB436', 'RA786', 'IX009', 'EE789')
+),
 
-SELECT *
-FROM {{ ref('silver_txn_full_posted_transaction') }}
-WHERE posting_transaction_code IN ('GL491', 'IZ106', 'UB436', 'RA786', 'IX009', 'EE789')
+filled_country AS (
+    SELECT
+        *,
+        CASE 
+            WHEN country_code IS NULL OR TRIM(country_code) = '' 
+                THEN posting_currency_code
+            ELSE country_code
+        END AS filled_country_code,
+
+        CASE 
+            WHEN mcc IS NULL OR TRIM(mcc) = '' 
+                THEN '6540'
+            ELSE mcc
+        END AS updated_mcc
+    FROM source_data
+),
+
+standardized AS (
+    SELECT
+        sd.* EXCEPT (country_code, mcc, filled_country_code, updated_mcc),
+        COALESCE(
+            cc.numeric_country_code,  
+            CASE 
+                WHEN REGEXP_LIKE(filled_country_code, '^[0-9]+$') THEN filled_country_code 
+                ELSE NULL
+            END
+        ) AS country_code,
+        updated_mcc AS mcc
+    FROM filled_country AS sd
+    LEFT JOIN {{ ref('silver_txn_country_code') }} AS cc
+        ON LOWER(sd.filled_country_code) IN (
+            LOWER(cc.alpha2_country_code),
+            LOWER(cc.alpha3_country_code)
+        )
 )
 
-select *
-from source_data
+SELECT *
+FROM standardized
